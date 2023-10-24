081d1c4a3a4b9c44c55e3f1b8abc6b0f02b999e8
Michał Winiecki <falrond@gmail.com>
2023-10-25 01:02:13 +0200

    add navbar component, style sidebar-item component

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 
 interface SidebarItemProps {
   icon: LucideIcon;
@@ -16,7 +17,7 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
 
   const isActive =
     (pathname === "/" && href === "/") ||
-    pathname === "href" ||
+    pathname === href ||
     pathname?.startsWith(`${href}/`);
 
   const onClick = () => {
@@ -28,7 +29,7 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
       onClick={onClick}
       type="button"
       className={cn(
-        "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+        "flex items-center h-full gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
         isActive &&
           "text-sky-700 bg-sky-200/20 hover:bg-sky-200/20 hover:text-sky-700"
       )}
@@ -40,6 +41,12 @@ export const SidebarItem = ({ icon: Icon, label, href }: SidebarItemProps) => {
         />
         {label}
       </div>
+      <div
+        className={cn(
+          "ml-auto opacity-0 border-2 border-sky-700 h-full transition-all",
+          isActive && "opacity-100"
+        )}
+      />
     </button>
   );
 };
